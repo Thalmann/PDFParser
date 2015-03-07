57e6747ea91282fbbe7aeedfbea64b837cf8b772
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-07 14:17:10 +0100

    new classes

---
+++ IKEAPDFPArser/IKEAPDFPArser/EventTypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEAPDFPArser
+{
+    public enum EventType
+    {
+        Standard,
+        Holiday,
+        Meeting
+    }
+    public static class EventTypeExtensions
+    {
+        public static string ToString(this EventType e)
+        {
+            switch (e)
+            {
+                case EventType.Standard: return "Arbejde IKEA";
+                case EventType.Holiday: return "Arbejde IKEA - Feriedag";
+                case EventType.Meeting: return "Arbejde IKEA - Møde";
+                default: throw new ArgumentException("Unknown enum of type: EventType.");
+            }
+        }
+    }
+}
